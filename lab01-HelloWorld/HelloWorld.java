f0538609f8955be51c8fa8f1839c7497c2b57c86
nam219 <nam219@lehigh.edu>
2015-08-27 15:02:17 +0000

    updated comment

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,28 @@
+/*
+Nick McIntyre
+Lab 01
+Hello World Java Program
+
+    first compile the program
+        javac HelloWorld.java
+    run the program
+        java HelloWorld
+*/
+
+//define a class
+public class HelloWorld
+{
+    //add main method
+    public static void main (String [] args)
+    {
+        System.out.println("#########################");
+        System.out.println("#########################");
+        System.out.println("#                       #");
+        System.out.println("#   Hello World 2015!!  #");
+        System.out.println("#                       #");
+        System.out.println("#########################");
+        System.out.println("#########################");
+        
+        //test/testtest
+    }
+}

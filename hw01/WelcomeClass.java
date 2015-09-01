3de3731733739966f54985a28cf4d3967ce6e50e
nam219 <nam219@lehigh.edu>
2015-09-01 20:52:51 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+/*
+Nick McIntyre
+Homwwoork 01
+Welcome Class Java Program
+
+    first compile the program
+        javac WelcomeClass.java
+    run the program
+        java WelcomeClass
+*/
+
+//define a class
+public class WelcomeClass
+{
+    //add main method
+    public static void main (String [] args)
+    {
+        //printing out my username and welcome along with a tweet long sentence about myself
+        System.out.println("    -----------");
+        System.out.println("    | WELCOME |");
+        System.out.println("    -----------");
+        System.out.println("    ^ ^ ^ ^ ^ ^");
+        System.out.println("   / \\/ \\/ \\/ \\");
+        System.out.println("|-N--A--M--2--1--9-|");
+        System.out.println("   \\ /\\ /\\ /\\ /");
+        System.out.println("    v v v v v v");
+        System.out.println("This summer I biked from Passau, Germany to Vienna, Austria with my grandparents and brother.");
+    }
+}

5bb40c82b41c93dd6b669d8a42b1601b4c85e251
nam219 <nam219@lehigh.edu>
2015-11-05 16:09:17 +0000

    updated arrays

--- lab11/arrays.java
+++ lab11/arrays.java
@@ -14,12 +14,20 @@ public class arrays
             arr[x] = (int)(Math.random()*100);
         }
         sort(arr);//sorts the array
+        for(int x = 0; x<arr.length; x++)//prints it
+        {
+            System.out.println(arr[x]);
+        }
         int arr2[] = new int[10];//new int array
         for(int x = 0; x<arr2.length; x++)//put random numbers in it
         {
             arr2[x] = (int)(Math.random()*100);
         }
         sort(arr2);//sorts it
+        for(int x = 0; x<arr2.length; x++)//prints it
+        {
+            System.out.println(arr2[x]);
+        }
         int arr3[] = new int[30];//new array to hold 1 and 2 combined
         for(int x = 0; x<arr.length; x++)//adds the first array to it
         {

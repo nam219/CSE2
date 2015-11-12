c001eaf2104bd0165179e7d1ff210dbb05702112
nam219 <nam219@lehigh.edu>
2015-11-12 16:08:39 +0000

    updated move_array

--- lab12/move_array.java
+++ lab12/move_array.java
@@ -26,7 +26,7 @@ public class move_array
         int array[] = new int[size];//creates the array
         for(int x = 0; x<array.length; x++)//populates it
             array[x] = (int)(Math.random()*100);
-        //System.out.print(Arrays.toString(array));
+        System.out.print("The current array is " + Arrays.toString(array) + "\n");
         System.out.println("Enter the index of the array to be moved");
         move = scan.nextInt();//what index will be moved
         try//makes sure its within the length-1 of the array
@@ -54,7 +54,7 @@ public class move_array
             }
             array[array.length-1] = x;*/
         }
-        //System.out.print(Arrays.toString(array));
+        System.out.print("Final array " + Arrays.toString(array));
     }
 
 }
